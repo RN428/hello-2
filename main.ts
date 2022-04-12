f4fceca029d0b9a57eb5fb3364666e1479721b5f
RN428 <103495323+RN428@users.noreply.github.com>
2022-04-12 11:31:30 +0200

    ksdvhd!fklh

--- main.ts
+++ main.ts
@@ -1,17 +1,16 @@
-if (input.buttonIsPressed(Button.A)) {
-    basic.clearScreen()
-    basic.showString("T=")
-    basic.showNumber(input.temperature())
-    basic.showString("DEGRES")
-} else {
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-}
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showNumber(input.temperature())
+        basic.showString("DEGRES")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
 })
